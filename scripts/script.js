b810fc88614ee4d49c916258ebb3b920f9e85ee1
Artur <a.kalynin@gmail.com>
2024-06-22 12:42:24 +0300

    Added one mini programm to script.js about films which users watched

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+let numberOfFilms = +prompt("How many films have you watched?");
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {
+        name = prompt("name of your film") : grade = +prompt("what is your grade about this film?")
+    },
+    actors : {},
+    genres : [],
+    privat : false
+}
