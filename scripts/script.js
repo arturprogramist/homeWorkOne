ceb690ad1db694ae28bac58c466bed328939367a
Artur <a.kalynin@gmail.com>
2024-06-22 13:21:06 +0300

    update code and fix mine problems

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,18 @@ let numberOfFilms = +prompt("How many films have you watched?");
 
 const personalMovieDB = {
     count : numberOfFilms,
-    movies : {
-        name = prompt("name of your film") : grade = +prompt("what is your grade about this film?")
-    },
+    movies : {},
     actors : {},
     genres : [],
     privat : false
 }
+let name = prompt("name of your film");
+let grade = +prompt("what is your grade about this film?")
+let name1 = prompt("name of your film");
+let grade1 = +prompt("what is your grade about this film?")
+
+personalMovieDB.movies[name] = grade;
+personalMovieDB.movies[name1] = grade1;
+
+
+console.log(personalMovieDB);
